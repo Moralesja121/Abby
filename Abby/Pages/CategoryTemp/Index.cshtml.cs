351f24b4e80219337e79b26dd0b6667baf99156d
Joshua Morales <jamorales121@gmail.com>
2021-12-05 23:41:46 -0500

    Scaffolded CRUD in CategoryTemp folder

---
+++ Abby/Pages/CategoryTemp/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Abby.Data;
+using Abby.Model;
+
+namespace Abby.Pages.CategoryTemp
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Abby.Data.ApplicationDbContext _context;
+
+        public IndexModel(Abby.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Category = await _context.Category.ToListAsync();
+        }
+    }
+}
