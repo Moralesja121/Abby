bce21111e732ce1da8d1541dcbf576775d244f1a
Joshua Morales <jamorales121@gmail.com>
2021-12-05 21:38:06 -0500

    Bootstrap changes, create page, and post binding

---
+++ Abby/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,31 @@
+using Abby.Data;
+using Abby.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Abby.Pages.Categories
+{
+    // [BindProperties] We can use this to auto-bind properties within this page/code-behind without have to type out [BindProperty for each one]
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await _db.Category.AddAsync(Category);
+            await _db.SaveChangesAsync(); // This command is the actual saving of data to the database
+            return RedirectToPage("Index");
+        }
+    }
+}
